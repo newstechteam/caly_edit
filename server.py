3e838df05740baa4d039404c236b9abdd7d16380
newstechteam <antsbs4@gmail.com>
2025-02-10 08:02:46 +0900

    Update server.py

--- server.py
+++ server.py
@@ -100,7 +100,7 @@ fcm_html = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>캘리리 알림 보내기</title>
+    <title>캘리_영상편집팀 알림 보내기</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body { background-color: #f8f9fa; display: flex; justify-content: center; align-items: center; height: 100vh; }
@@ -111,7 +111,7 @@ fcm_html = """
 </head>
 <body>
     <div class="container">
-        <h3 class="text-center mb-4">🔥 Caly 알림 보내기</h3>
+        <h3 class="text-center mb-4">🔥 Caly_영상편집팀 알림 보내기</h3>
         <form id="notificationForm">
             <div class="mb-3">
                 <label class="form-label">제목</label>
